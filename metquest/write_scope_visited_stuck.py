9933e23452a34660f87ed61505788d82bba397d4
Dinesh Kumar K B <bt19s011@smail.iitm.ac.in>
2022-09-24 23:32:35 +0530

    added scripts for metadata
    Extra scripts to find metadata were added

---
+++ metquest/write_scope_visited_stuck.py
@@ -0,0 +1,35 @@
+# Writes all the scope, visited rxns and stuck rxns of all microbes in separate files
+import metquest as mq
+import cobra
+import glob
+import os
+import pandas as pd
+
+path = r'C:\Users\dines\Documents\plant-microbiome\models\R2A'
+seedmet_file = r'C:\Users\dines\Documents\plant-microbiome\models\R2A\R2A_edition.txt'
+os.chdir(path)
+
+file_names = glob.glob('*.xml')
+if not file_names:
+    file_names = glob.glob('*.sbml')
+
+if not file_names:
+    print("There are no sbml files. Please check the path")
+
+print("Filenames", file_names)
+
+model = []
+for i in range(len(file_names)):
+    model.append(cobra.io.read_sbml_model(file_names[i]))
+
+org_info, scope, full_name_map, vis = mq.pairwiseMSI.find_stuck_rxns(model, file_names, seedmet_file, 1)
+
+org_info_decrypt = mq.pairwiseMSI.decrypt_org_info(org_info, full_name_map)
+vis_decrypt = mq.pairwiseMSI.decrypt_org_info(vis, full_name_map)
+
+df1 = pd.DataFrame.from_dict(org_info_decrypt, orient="index")
+df1.T.to_csv('./results/'+os.path.basename(seedmet_file).replace('.txt', '_stuckrxns.csv'))
+df2 = pd.DataFrame.from_dict(scope, orient="index")
+df2.T.to_csv('./results/'+os.path.basename(seedmet_file).replace('.txt', '_scope.csv'))
+df3 = pd.DataFrame.from_dict(vis_decrypt, orient="index")
+df3.T.to_csv('./results/'+os.path.basename(seedmet_file).replace('.txt', '_visitedrxns.csv'))
---
+++ metquest/write_visited_stuck_num.py
@@ -0,0 +1,35 @@
+# Writes the number of visited rxns and number of stuck rxns of all microbes in a file
+import metquest as mq
+import cobra
+import glob
+import os
+import pandas as pd
+
+path = r'C:\Users\dines\Documents\plant-microbiome\models\R2A'
+seedmet_file = r'C:\Users\dines\Documents\plant-microbiome\models\R2A\R2A_edition.txt'
+os.chdir(path)
+
+file_names = glob.glob('*.xml')
+if not file_names:
+    file_names = glob.glob('*.sbml')
+
+if not file_names:
+    print("There are no sbml files. Please check the path")
+
+print("Filenames", file_names)
+
+model = []
+for i in range(len(file_names)):
+    model.append(cobra.io.read_sbml_model(file_names[i]))
+
+org_info, _, full_name_map, vis = mq.pairwiseMSI.find_stuck_rxns(model, file_names, seedmet_file, 1)
+
+organism_stats = {}
+for org in org_info:
+    organism_stats[org] = []
+    organism_stats[org].append(len(list(set(org_info[org]))))
+    organism_stats[org].append(len(list(set(vis[org]))))
+
+df1 = pd.DataFrame.from_dict(organism_stats, orient="index")
+df1.columns = ['Stuck rxns', 'Visited rxns']
+df1.to_csv('./results/organism_stats_'+os.path.basename(seedmet_file).replace('.txt', '.csv'))
